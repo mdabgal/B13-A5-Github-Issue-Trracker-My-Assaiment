fe191da595ca8efe919edabbef6f6fd7655d177b
Abgal <hmdabgal@gmail.com>
2026-03-07 10:45:46 +0600

    Added for login js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 
 
+const demoUsername = "admin";
+const demoPassword = "admin123";
+
+const form = document.getElementById("loginForm");
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+if(username === demoUsername && password === demoPassword){
+    alert("Login Successfull !");
+}
+else{
+    alert("Invalid credentials!");
+}
+});
